b0ffb78b9aa1afe89bc80660eeaad07249ec699d
Dima-Filanovich <108800726+Dima-Filanovich@users.noreply.github.com>
2025-05-26 00:17:18 +0300

    Добавил структуру и регистрацию2

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,15 +1,20 @@
 import hashlib
 from models.user_model import create_user, get_user
 
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
+class AuthController:
+    def __init__(self):
+        pass
 
-def register_user(username, password):
-    password_hash = hash_password(password)
-    return create_user(username, password_hash)
+    @staticmethod
+    def hash_password(password):
+        return hashlib.sha256(password.encode()).hexdigest()
 
-def login_user(username, password):
-    user = get_user(username)
-    if user and user[2] == hash_password(password):
-        return {"id": user[0], "username": user[1]}
-    return None
+    def register_user(self, username, password):
+        password_hash = self.hash_password(password)
+        return create_user(username, password_hash)
+
+    def login_user(self, username, password):
+        user = get_user(username)
+        if user and user[2] == self.hash_password(password):
+            return {"id": user[0], "username": user[1]}
+        return None
--- controllers/predict_controller.py
+++ controllers/predict_controller.py
@@ -4,51 +4,50 @@ from PIL import Image
 from keras.layers import TFSMLayer
 from models.history_model import add_history
 
-# Подгрузка модели
-model = TFSMLayer("food101_modelon", call_endpoint="serving_default")
+class PredictController:
+    def __init__(self):
+        self.model = TFSMLayer("food101_modelon", call_endpoint="serving_default")
+        self.CLASS_NAMES = [
+            'apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare',
+            'beet_salad', 'beignets', 'bibimbap', 'bread_pudding', 'breakfast_burrito',
+            'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake',
+            'ceviche', 'cheesecake', 'cheese_plate', 'chicken_curry', 'chicken_quesadilla',
+            'chicken_wings', 'chocolate_cake', 'chocolate_mousse', 'churros', 'clam_chowder',
+            'club_sandwich', 'crab_cakes', 'creme_brulee', 'croque_madame', 'cup_cakes',
+            'deviled_eggs', 'donuts', 'dumplings', 'edamame', 'eggs_benedict',
+            'escargots', 'falafel', 'filet_mignon', 'fish_and_chips', 'foie_gras',
+            'french_fries', 'french_onion_soup', 'french_toast', 'fried_calamari', 'fried_rice',
+            'frozen_yogurt', 'garlic_bread', 'gnocchi', 'greek_salad', 'grilled_cheese_sandwich',
+            'grilled_salmon', 'guacamole', 'gyoza', 'hamburger', 'hot_and_sour_soup',
+            'hot_dog', 'huevos_rancheros', 'hummus', 'ice_cream', 'lasagna',
+            'lobster_bisque', 'lobster_roll_sandwich', 'macaroni_and_cheese', 'macarons', 'miso_soup',
+            'mussels', 'nachos', 'omelette', 'onion_rings', 'oysters',
+            'pad_thai', 'paella', 'pancakes', 'panna_cotta', 'peking_duck',
+            'pho', 'pizza', 'pork_chop', 'poutine', 'prime_rib',
+            'pulled_pork_sandwich', 'ramen', 'ravioli', 'red_velvet_cake', 'risotto',
+            'samosa', 'sashimi', 'scallops', 'seaweed_salad', 'shrimp_and_grits',
+            'spaghetti_bolognese', 'spaghetti_carbonara', 'spring_rolls', 'steak', 'strawberry_shortcake',
+            'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna_tartare',
+            'waffles'
+        ]
 
-# Классы
-CLASS_NAMES = [
-    'apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare',
-    'beet_salad', 'beignets', 'bibimbap', 'bread_pudding', 'breakfast_burrito',
-    'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake',
-    'ceviche', 'cheesecake', 'cheese_plate', 'chicken_curry', 'chicken_quesadilla',
-    'chicken_wings', 'chocolate_cake', 'chocolate_mousse', 'churros', 'clam_chowder',
-    'club_sandwich', 'crab_cakes', 'creme_brulee', 'croque_madame', 'cup_cakes',
-    'deviled_eggs', 'donuts', 'dumplings', 'edamame', 'eggs_benedict',
-    'escargots', 'falafel', 'filet_mignon', 'fish_and_chips', 'foie_gras',
-    'french_fries', 'french_onion_soup', 'french_toast', 'fried_calamari', 'fried_rice',
-    'frozen_yogurt', 'garlic_bread', 'gnocchi', 'greek_salad', 'grilled_cheese_sandwich',
-    'grilled_salmon', 'guacamole', 'gyoza', 'hamburger', 'hot_and_sour_soup',
-    'hot_dog', 'huevos_rancheros', 'hummus', 'ice_cream', 'lasagna',
-    'lobster_bisque', 'lobster_roll_sandwich', 'macaroni_and_cheese', 'macarons', 'miso_soup',
-    'mussels', 'nachos', 'omelette', 'onion_rings', 'oysters',
-    'pad_thai', 'paella', 'pancakes', 'panna_cotta', 'peking_duck',
-    'pho', 'pizza', 'pork_chop', 'poutine', 'prime_rib',
-    'pulled_pork_sandwich', 'ramen', 'ravioli', 'red_velvet_cake', 'risotto',
-    'samosa', 'sashimi', 'scallops', 'seaweed_salad', 'shrimp_and_grits',
-    'spaghetti_bolognese', 'spaghetti_carbonara', 'spring_rolls', 'steak', 'strawberry_shortcake',
-    'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna_tartare',
-    'waffles'
-]
+    def preprocess_image(self, image: Image.Image) -> np.ndarray:
+        image = image.resize((224, 224))
+        img_array = tf.keras.preprocessing.image.img_to_array(image)
+        img_array = tf.keras.applications.efficientnet.preprocess_input(img_array)
+        return np.expand_dims(img_array, axis=0)
 
-def preprocess_image(image: Image.Image) -> np.ndarray:
-    image = image.resize((224, 224))
-    img_array = tf.keras.preprocessing.image.img_to_array(image)
-    img_array = tf.keras.applications.efficientnet.preprocess_input(img_array)
-    return np.expand_dims(img_array, axis=0)
+    def predict(self, image: Image.Image, user_id: int = None, image_name: str = ""):
+        img_tensor = tf.convert_to_tensor(self.preprocess_image(image))
+        output_dict = self.model(img_tensor)
+        predictions = list(output_dict.values())[0].numpy()[0]
 
-def predict(image: Image.Image, user_id: int, image_name: str):
-    img_tensor = tf.convert_to_tensor(preprocess_image(image))
-    output_dict = model(img_tensor)
-    predictions = list(output_dict.values())[0].numpy()[0]
-    
-    top_indices = predictions.argsort()[-3:][::-1]
-    top_classes = [CLASS_NAMES[i].replace("_", " ").title() for i in top_indices]
-    confidences = [predictions[i] for i in top_indices]
+        top_indices = predictions.argsort()[-3:][::-1]
+        top_classes = [self.CLASS_NAMES[i].replace("_", " ").title() for i in top_indices]
+        confidences = [predictions[i] for i in top_indices]
 
-    # Сохраняем предсказание в историю
-    if user_id:
-        add_history(user_id, image_name, top_classes[0], confidences[0])
+        if user_id:
+            add_history(user_id, image_name, top_classes[0], confidences[0])
 
-    return top_classes, confidences
+        return top_classes, confidences
+
